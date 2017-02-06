23e2a737d348de31e5da22710958fb9b3a2aba65
mohancm100 <mohanmanjappa@gmail.com>
2017-02-06 22:28:43 +0530

    Typo error

--- board/kernel.mk
+++ board/kernel.mk
@@ -5,7 +5,7 @@ BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2 androidboot.selinux=permissive an
 BOARD_MKBOOTIMG_ARGS := --base 0x40078000 --pagesize 2048 --board 1465391499 --ramdisk_offset 0x03f88000 --second_offset 0x40f0000 --tags_offset 0x0df88000 --board mohan
 
 # Kernel properties
-TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilts/zlmage-dtb.gz
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilts/zlmage.gz-dtb
 
 # Hack for building without kernel sources
 $(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
